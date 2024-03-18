388a9927754994bdd4d71c52cb3e90e55a05bc87
LehlohonoloKhoathane <140171463+LehlohonoloKhoathane@users.noreply.github.com>
2024-03-18 11:37:24 +0200

    sass packages and header

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+import styles from './Header.module.scss';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return (
-    <div>
-      Header
+  return <header>
+    <div className={styles.header}>
+        <div className={styles.logo}>
+            <Link to='/'>
+                <h2>My <span>Shop</span></h2>
+            </Link>
+        </div>
     </div>
-  )
-}
+    </header>;
+};
 
-export default Header
+export default Header;
