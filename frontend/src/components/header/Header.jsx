84fb17ba790703a303470acc6f8720e13bb8e86e
LehlohonoloKhoathane <140171463+LehlohonoloKhoathane@users.noreply.github.com>
2024-03-18 11:57:19 +0200

    header

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import styles from './Header.module.scss';
 import { Link } from 'react-router-dom';
 
+const logo = (
+    <div className={styles.logo}>
+        <Link to="/">
+            <h2>My<span>Shop</span></h2>
+        </Link>
+    </div>
+)
+
 const Header = () => {
   return <header>
-    <div className={styles.header}>
-        <div className={styles.logo}>
-            <Link to='/'>
-                <h2>My <span>Shop</span></h2>
-            </Link>
-        </div>
-    </div>
+    <div className={styles.header}>{logo}</div>
     </header>;
 };
 
