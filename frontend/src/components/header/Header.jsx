6b6f24c8cf743246b92481a247ae4e56dfdc9a28
LehlohonoloKhoathane <140171463+LehlohonoloKhoathane@users.noreply.github.com>
2024-03-18 12:16:00 +0200

    header additions

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import styles from './Header.module.scss';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
-const logo = (
+export const logo = (
     <div className={styles.logo}>
         <Link to="/">
             <h2>My<span>Shop</span></h2>
         </Link>
     </div>
-)
+);
+
+const activeLink = ({isActive}) => (isActive ? `${styles.active}` : "")
 
 const Header = () => {
-  return <header>
-    <div className={styles.header}>{logo}</div>
-    </header>;
+  return (
+  <header>
+    <div className={styles.header}>{logo}
+    <nav>
+        <ul>
+            <li>
+                <NavLink to="/shop" className={activeLink}/>
+            </li>
+        </ul>
+        <div className={styles["header-right"]}></div>
+    </nav>
+    </div>
+    </header>
+  );
 };
 
 export default Header;
