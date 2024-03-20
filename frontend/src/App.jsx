c83651777ce189037b25958516e5ba4fda6e341b
LehlohonoloKhoathane <140171463+LehlohonoloKhoathane@users.noreply.github.com>
2024-03-20 12:14:10 +0200

    footer and slider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 // import './App.css'
 import Home from './pages/home/home';
 import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>}/>
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </>
   )
--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import styles from './Footer.module.scss';
 
 const Footer = () => {
+    const date = new Date();
+    const year = date.getFullYear();
   return (
     <div className={styles.footer}>
-      
+      Copyright &copy; {year} All Rights Reserved | Designed and Developed by Lehlohonolo Khoathane
     </div>
   )
 }
