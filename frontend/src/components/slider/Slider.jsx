dd8859b63110520e4489a4ed7243f5ed0301c670
LehlohonoloKhoathane <140171463+LehlohonoloKhoathane@users.noreply.github.com>
2024-03-20 13:23:39 +0200

    slider additions

--- frontend/src/components/slider/Slider.jsx
+++ frontend/src/components/slider/Slider.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 import myPic from "../../assets/images/iphone-removebg-preview.png";
 import image1 from "../../assets/images/jackets-removebg-preview.png";
 import image2 from "../../assets/images/ladiescoats-removebg-preview.png";
@@ -10,3 +12,14 @@ import image8 from "../../assets/images/underArmourCap-removebg-preview.png";
 import image9 from "../../assets/images/watch-removebg-preview.png";
 
 
+import React from 'react'
+
+const Slider = () => {
+  return (
+    <div className="slider">
+      
+    </div>
+  )
+}
+
+export default Slider
