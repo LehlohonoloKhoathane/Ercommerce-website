5f9fdf62c758d05aee4db5414d1a307ef53c748e
LehlohonoloKhoathane <140171463+LehlohonoloKhoathane@users.noreply.github.com>
2024-03-20 13:09:38 +0200

    picures of the sliders

---
+++ frontend/src/components/slider/Slider.jsx
@@ -0,0 +1,12 @@
+import myPic from "../../assets/images/iphone-removebg-preview.png";
+import image1 from "../../assets/images/jackets-removebg-preview.png";
+import image2 from "../../assets/images/ladiescoats-removebg-preview.png";
+import image3 from "../../assets/images/noke_hoodie-removebg-preview.png";
+import image4 from "../../assets/images/perfume-removebg-preview.png";
+import image5 from "../../assets/images/smart_watch-removebg-preview.png";
+import image6 from "../../assets/images/suit-removebg-preview.png";
+import image7 from "../../assets/images/sweater-removebg-preview.png";
+import image8 from "../../assets/images/underArmourCap-removebg-preview.png";
+import image9 from "../../assets/images/watch-removebg-preview.png";
+
+
