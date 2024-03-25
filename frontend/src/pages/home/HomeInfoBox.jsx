bed8683ed86eb4a1d4963f43914cd27383d7505f
LehlohonoloKhoathane <140171463+LehlohonoloKhoathane@users.noreply.github.com>
2024-03-25 13:17:10 +0200

    home page additions

---
+++ frontend/src/pages/home/HomeInfoBox.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { FaShippingFast } from "react-icons/fa";
+import { BsFillCreditCardFill } from "react-icons/bs"
+import {}
+
+const data = [
+    {
+      icon: <FaShippingFast size={30} color="#8cb4f5" />,
+      heading: "Free Shipping",
+      text: "We offer free shipping on special products",
+    },
+    {
+      icon: <BsFillCreditCardFill size={30} color="#f7d272" />,
+      heading: "Secure Payment",
+      text: "Make secure payment for your product.",
+    },
+    {
+      icon: <BsCartCheck size={30} color="#fa82ea" />,
+      heading: "Quality Products",
+      text: "We sell products from only tested and proven brands.",
+    },
+    {
+      icon: <BsClockHistory size={30} color="#82fa9e" />,
+      heading: "24/7 Support",
+      text: "Get access to support from our exprt support team.",
+    },
+  ];
+
+const HomeInfoBox = () => {
+  return (
+    <div>
+      HomeInfoBox
+    </div>
+  )
+}
+
+export default HomeInfoBox
