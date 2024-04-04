440edd4436bace58d43206c69ea0a7fb163e7379
LehlohonoloKhoathane <140171463+LehlohonoloKhoathane@users.noreply.github.com>
2024-04-04 11:54:35 +0000

    more routes and controllers

---
+++ backend/controllers/userController.js
@@ -0,0 +1,10 @@
+const asyncHandler = require("express-async-handler");
+
+//registering the user
+const registerUser = asyncHandler(async (req, res) => {
+    res.send("Register User...")
+});
+
+module.exports = {
+    registerUser
+}
