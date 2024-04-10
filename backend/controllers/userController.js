1fca979ac36094126b4cf65273060aad77ae8586
LehlohonoloKhoathane <140171463+LehlohonoloKhoathane@users.noreply.github.com>
2024-04-10 19:40:22 +0000

    validation function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,6 +4,18 @@ const bcrypt = require("bcryptjs");
 
 //registering the user
 const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+
+    //Validations
+    if(!name || !email || !password){
+        res.status(400)
+        throw newError("Please fill in all required fields");
+    }
+    if(password.length < 6){
+        res.status(400);
+        throw newError("Password must be up to 6 characters");
+    }
+    
     res.send("Register User...")
 });
 
