ae16777efb692053738235a67685fbbb34cabce9
LehlohonoloKhoathane <140171463+LehlohonoloKhoathane@users.noreply.github.com>
2024-04-10 19:54:43 +0000

    jws web token generator

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,13 @@
 const asyncHandler = require("express-async-handler");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const User = require("../models/userModel");
+
+const generateToken = () => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn: "1d"
+    })
+}
 
 //registering the user
 const registerUser = asyncHandler(async (req, res) => {
@@ -15,6 +22,20 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(400);
         throw newError("Password must be up to 6 characters");
     }
+
+    //check if user exists
+    const userExists = await User.findOne({email: email})
+    if (userExists){
+        res.status(400);
+        throw new Error("Email has already been registered");
+    }
+
+    //creating new user
+    const user = await User.create({
+        name, 
+        email,
+        password
+    })
     
     res.send("Register User...")
 });
