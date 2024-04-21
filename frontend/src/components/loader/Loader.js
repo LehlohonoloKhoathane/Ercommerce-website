a868640c874585f373841aad53fbbfa508b0d959
LehlohonoloKhoathane <140171463+LehlohonoloKhoathane@users.noreply.github.com>
2024-04-21 13:01:51 +0000

    loader

--- frontend/src/components/loader/Loader.js
+++ frontend/src/components/loader/Loader.js
@@ -1,23 +1,12 @@
 import React from "react";
 import styles from "./Loader.module.scss";
 import ReactDOM from "react-dom";
-import loadingImg from "../../assets/images/myshop.gif";
+import 
 
 const Loader = () => {
     return ReactDOM.createPortal(
         <div className={styles.wrapper}>
-            <div className={styles.loader}>
-                <img src={loadingImg} alt="loading" />
-            </div>
-        </div>,
-        document.getElementById("loader")
-    );
-};
-
-export const Spinner = () => {
-    return (
-        <div className="--center-all">
-            <img src={loadingImg} alt="loading" width={50} />
+            <div className={styles.loader}></div>
         </div>
     )
 }
