9b9f4da1eceb43bad0b148bd31a78d900258e93d
LehlohonoloKhoathane <khoathanelehlohonolo@gmail.com>
2024-05-14 22:28:44 +0200

    js to jsx

--- frontend/src/components/loader/Loader.jsx
+++ frontend/src/components/loader/Loader.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import styles from "./Loader.module.scss";
 import ReactDOM from "react-dom";
-import 
+import styles from "./Loader.module.scss";
 
 const Loader = () => {
     return ReactDOM.createPortal(
         <div className={styles.wrapper}>
             <div className={styles.loader}></div>
-        </div>
-    )
-}
+        </div>,
+        document.body // specify the DOM element to append the loader to
+    );
+};
 
 export default Loader;
