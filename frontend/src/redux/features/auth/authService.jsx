a35329c18b4477b80ebad7d946b3e57c9cf426b1
LehlohonoloKhoathane <khoathanelehlohonolo@gmail.com>
2024-05-20 21:02:11 +0200

    login login and functions

--- frontend/src/redux/features/auth/authService.jsx
+++ frontend/src/redux/features/auth/authService.jsx
@@ -11,8 +11,14 @@ const register = async (userData) => {
     return response.data;
 };
 
+//Login user
+const login = async (userData) => {
+    const response = await axios.post(API_URL + "login", userData);
+    return response.data;
+};
+
 const authService = {
-    register
+    register, login
 };
 
 export default authService;
--- frontend/src/redux/features/auth/authSlice.jsx
+++ frontend/src/redux/features/auth/authSlice.jsx
@@ -27,7 +27,25 @@ export const register = createAsyncThunk(
                 return thunkAPI.rejectWithValue(message);
         }
     }
-)
+);
+
+//login user
+export const login = createAsyncThunk(
+    "auth/login",
+    async (userData, thunkAPI) => {
+        try {
+            return await authService.login(userData)
+        } catch (error) {
+            const message = 
+            (error.response &&
+                error.response.data &&
+                error.response.data.message) ||
+                error.message ||
+                error.toString();
+                return thunkAPI.rejectWithValue(message);
+        }
+    }
+);
 
 const authSlice = createSlice({
     name: "auth",
@@ -52,6 +70,25 @@ const authSlice = createSlice({
             state.isLoggedIn = true;
             state.user = action.payload
             toast.success("Registration successful")
+        })
+        .addCase(register.rejected, (state, action) =>{
+            state.isLoading = false;
+            state.isError = true;
+            state.message = action.payload;
+            state.user = null;
+            toast.success(action.payload);
+        })
+
+        //login User
+        .addCase(login.pending, (state) =>{
+            state.isLoading = true;
+        })
+        .addCase(login.fulfilled, (state, action) =>{
+            state.isLoading = false;
+            state.isSuccess = true;
+            state.isLoggedIn = true;
+            state.user = action.payload
+            toast.success("Login successful")
         })
         .addCase(register.rejected, (state, action) =>{
             state.isLoading = false;
