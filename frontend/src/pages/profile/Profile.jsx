875dbfb872e79e14a67bfd4585f6d60d2444dce2
LehlohonoloKhoathane <khoathanelehlohonolo@gmail.com>
2024-06-04 15:22:39 +0200

    adding a profile

--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Profile.scss";
 import PageMenu from "../../components/pageMenu/PageMenu";
+import { useSelector } from "react-redux";
+import Card from "../../components/card/Card"
+
 
 const Profile = () => {
+    const [profile, setProfile] = useState(initialState);
+    const { isLoading, user } = useSelector(
+        (state) => state.auth
+    );
+
+    const initialState = {
+        name: user?.name || "",
+        email: user?.name || "",
+        phone: user?.name || "",
+        role: user?.name || "",
+        address: user?.name || {},
+    }
+
+    const saveProfile = async () => {};
 
     return (
         <>
@@ -10,6 +27,18 @@ const Profile = () => {
             <div className="container">
                 <PageMenu/>
                 <h2>Profile</h2>
+                <div className="--flex-start profile">
+                    <Card cardClass={"card"}>
+                        {!isLoading && user && (
+                            <>
+                                <div className="profile-photo">
+                                    <h2>Profile Image</h2>
+                                </div>
+                                <form onSubmit={saveProfile}></form>
+                            </>
+                        )}
+                    </Card>
+                </div>
             </div>
         </section>
         </>
