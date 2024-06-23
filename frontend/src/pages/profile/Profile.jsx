084b442b9ed64cf4c29a22b9b75ddd1ec2858312
LehlohonoloKhoathane <khoathanelehlohonolo@gmail.com>
2024-06-23 20:59:52 +0200

    profile photo upload to database

--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -3,9 +3,13 @@ import "./Profile.scss";
 import PageMenu from "../../components/pageMenu/PageMenu";
 import { useDispatch, useSelector } from "react-redux";
 import Card from "../../components/card/Card"
-import { getUser, updateUser } from "../../redux/features/auth/authSlice";
+import { getUser, updatePhoto, updateUser } from "../../redux/features/auth/authSlice";
 import { WiCloudUp } from "react-icons/wi";
+import { toast } from "react-toastify";
 
+const cloud_name = process.env.REACT_APP_CLOUD_NAME;
+const upload_preset = process.env.REACT_APP_UPLOAD_PRESET;
+const url = "https://api.cloudinary.com/v1_1/myName/image/upload";
 
 const Profile = () => {
     const { isLoading, user } = useSelector(
@@ -77,7 +81,37 @@ const Profile = () => {
         await dispatch(updateUser(userData));
     };
 
-    const savePhoto = async () => {};
+    const savePhoto = async (e) => {
+        e.preventDefault();
+        let imageURL;
+
+        try {
+            if(
+                profileImage !== null && (profileImage.type === "image/jpeg" || profileImage.type === "image/jpg" ||  profileImage.type === "image/png" )
+            ){
+                const image = new FormData()
+                image.append("file", profileImage)
+                image.append("cloud_name", cloud_name)
+                image.append("upload_preset", upload_preset)
+
+                //Saving image to cloudinary
+                const response = await fetch(url, {method: "post", body: image});
+                const imgData = await response.json();
+                //console.log(imgData);
+                imageURL = imgData.url.toString()
+            }
+
+            //Saving image to mongoDB
+            const userData = {
+                photo: profileImage ? imageURL : profile.photo
+            }
+            await dispatch(updatePhoto(userData));
+            setImagePreview(null)
+
+        } catch (error) {
+            toast.error(error.message);
+        }
+    };
 
 
     return (
